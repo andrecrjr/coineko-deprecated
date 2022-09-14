b826284ef2b415cadcfece4e3786ef0d8da44fc2
André Carlos Jr <andre-carlos@live.com>
2022-09-13 23:41:14 -0300

    - add snapshot

--- src/__tests__/pages/index.spec.tsx
+++ src/__tests__/pages/index.spec.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { ReactElement } from "react";
+import { render, screen, RenderResult } from "@testing-library/react";
 import App from "../../App";
 import { describe, test, expect, beforeEach, vi } from "vitest";
 import cryptoListMock from "../../__mocks__/cryptocurrency.mock.json";
@@ -25,7 +26,7 @@ vi.mock("axios", async () => {
 });
 
 describe("Main App test", () => {
-  let mainApp;
+  let mainApp: RenderResult;
 
   beforeEach(() => {
     mainApp = render(<App />);
@@ -36,6 +37,15 @@ describe("Main App test", () => {
   });
 
   test("should list one crypto", async () => {
-    expect((await screen.findByText("Bitcoin")).textContent).toBeDefined();
+    expect((await screen.findByText("Bitcoin")).textContent).toBe("Bitcoin");
+    expect((await screen.findByText("BTC")).textContent).toBe("BTC");
+    expect((await screen.findByText("BTC")).textContent).not.toBe("ETH");
+    expect(
+      (await screen.findByText("$67,916,847,953.00")).textContent
+    ).toBeDefined();
+  });
+
+  test("should snap all cryptos in home", () => {
+    expect(mainApp.container.children[0]).toMatchSnapshot();
   });
 });
