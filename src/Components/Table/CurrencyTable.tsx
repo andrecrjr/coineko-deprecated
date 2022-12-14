d06e526732524034044dc21129b0f994df963337
André Carlos Jr <andre-carlos@live.com>
2022-12-14 20:50:42 -0300

    - divide into small components

---
+++ src/Components/Table/CurrencyTable.tsx
@@ -0,0 +1,148 @@
+import { useState } from 'react';
+import { Currency } from 'src/Types';
+import { formatterMoney } from 'src/utils';
+import Star from './star.svg?component';
+
+const CurrencyTable = ({ currencyList }: { currencyList: Currency[] }) => {
+	const skeletonArray50ByPage = Array.from(Array(50).keys());
+	if (currencyList.length > 0)
+		return (
+			<>
+				{currencyList.map((currency: Currency) => (
+					<CurrencyChild key={currency.name} currency={currency} />
+				))}
+			</>
+		);
+	return (
+		<>
+			{skeletonArray50ByPage.map((item) => (
+				<CurrencyChild key={item} />
+			))}
+		</>
+	);
+};
+
+export const CurrencyChild = ({ currency }: { currency?: Currency }) => {
+	const [Favorite, setFavorite] = useState(false);
+	return (
+		<tr className="table--body__line pt-4">
+			<td className="table--body w-[35px] mr-15">
+				<span>
+					<Star
+						className={`w-5 ${Favorite ? 'fill-purple-neko' : 'fill-[none]'}`}
+						onClick={(e) => {
+							e.preventDefault();
+							setFavorite((state) => !state);
+						}}
+						data-testid="favorite-crypto"
+					/>
+				</span>
+			</td>
+
+			<ColumnCurrencyInfoGrid currency={currency} />
+			<ColumnMoneyFormatter
+				classNames={'table--body overflow-scroll sm:overflow-auto'}
+				formatPrice={currency?.current_price || 0}
+			/>
+
+			<ColumnPercentageCurrency
+				currencyNumber={currency?.price_change_percentage_1h_in_currency}
+			/>
+			<ColumnPercentageCurrency
+				currencyNumber={currency && currency?.market_cap_change_percentage_24h}
+			/>
+			<ColumnPercentageCurrency
+				currencyNumber={currency?.price_change_percentage_7d_in_currency}
+			/>
+			<ColumnMoneyFormatter
+				classNames="table--body"
+				formatPrice={currency?.market_cap || 0}
+			/>
+		</tr>
+	);
+};
+
+const ColumnCurrencyInfoGrid = ({
+	currency
+}: {
+	currency: Currency | undefined;
+}) => {
+	return (
+		<>
+			<td
+				className="table--body text-left
+			text-dark-purple-neko font-bold overflow-scroll sm:overflow-auto pl-3 "
+			>
+				{currency?.market_cap_rank || '...'}
+			</td>
+			<td className="table--body table--body__coin">
+				<section className="grid grid-cols-[auto_1fr] auto-rows-max">
+					<img
+						src={`${
+							currency?.image.replace('large', 'thumb') ||
+							'https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579'
+						}`}
+						className="mx-auto mt-auto"
+						style={{ userSelect: 'none' }}
+						width="25"
+						height="25"
+						alt={currency?.name}
+					/>
+					<a
+						className="row-span-2 w-max flex 
+                            items-center pl-3 max-w-[150px] font-bold 
+                             break-words overflow-scroll sm:overflow-hidden"
+						style={{ userSelect: 'none' }}
+					>
+						{currency?.name || 'Loading....'}
+					</a>
+					<p
+						className="text-[10px] text-center 
+        text-dark-purple-neko font-bold max-w-[auto] break-words"
+					>
+						{currency?.symbol.toUpperCase() || '...'}
+					</p>
+				</section>
+			</td>
+		</>
+	);
+};
+
+const ColumnMoneyFormatter = ({
+	classNames,
+	formatPrice
+}: {
+	classNames: string;
+	formatPrice: number;
+}) => {
+	return (
+		<td className={classNames}>
+			{formatterMoney(
+				'en-US',
+				{
+					style: 'currency',
+					currency: 'USD'
+				},
+				formatPrice
+			)}
+		</td>
+	);
+};
+
+const ColumnPercentageCurrency = ({
+	currencyNumber
+}: {
+	currencyNumber: number | undefined;
+}) => {
+	return (
+		<td
+			className={`table--body  ${
+				currencyNumber && currencyNumber > 0 ? 'text-green-500' : 'text-red-600'
+			}`}
+		>
+			{(currencyNumber && currencyNumber.toFixed(2)) || '0.00'}%
+		</td>
+	);
+};
+
+export default CurrencyTable;
--- src/Components/Table/index.tsx
+++ src/Components/Table/index.tsx
@@ -1,11 +1,10 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
-import Star from './star.svg?component';
 import Cat from 'src/assets/cat.svg?component';
 import { axiosInstance } from 'src/Services/ApiService';
-import { CurrencyList, Currency } from 'src/Types';
-import { formatterMoney } from 'src/utils';
+import { CurrencyList } from 'src/Types';
 import Pagination from './Pagination';
 import { FilterGlobalContext, PaginationContext } from 'src/Contexts';
+import CurrencyTable from './CurrencyTable';
 
 interface Props {
 	description: string;
@@ -78,135 +77,5 @@ export const Table = ({ description, category }: Props) => {
 				<Pagination />
 			</section>
 		</section>
-	);
-};
-
-const CurrencyTable = ({ currencyList }: { currencyList: Currency[] }) => {
-	const skeletonArray50ByPage = Array.from(Array(50).keys());
-	if (currencyList.length > 0)
-		return (
-			<>
-				{currencyList.map((currency: Currency) => (
-					<CurrencyChild key={currency.name} currency={currency} />
-				))}
-			</>
-		);
-	return (
-		<>
-			{skeletonArray50ByPage.map((item) => (
-				<CurrencyChild key={item} />
-			))}
-		</>
-	);
-};
-
-export const CurrencyChild = ({ currency }: { currency?: Currency }) => {
-	const [Favorite, setFavorite] = useState(false);
-	return (
-		<tr className="table--body__line pt-4">
-			<td className="table--body w-[35px] mr-15">
-				<span>
-					<Star
-						className={`w-5 ${Favorite ? 'fill-purple-neko' : 'fill-[none]'}`}
-						onClick={(e) => {
-							e.preventDefault();
-							setFavorite((state) => !state);
-						}}
-						data-testid="favorite-crypto"
-					/>
-				</span>
-			</td>
-			<td
-				className="table--body text-left
-			text-dark-purple-neko font-bold overflow-scroll sm:overflow-auto pl-3 "
-			>
-				{currency?.market_cap_rank || '...'}
-			</td>
-			<td className="table--body table--body__coin">
-				<section className="grid grid-cols-[auto_1fr] auto-rows-max">
-					<img
-						src={`${
-							currency?.image.replace('large', 'thumb') ||
-							'https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579'
-						}`}
-						className="mx-auto mt-auto"
-						style={{ userSelect: 'none' }}
-						width="25"
-						height="25"
-						alt={currency?.name}
-					/>
-					<a
-						className="row-span-2 w-max flex 
-										items-center pl-3 max-w-[150px] font-bold 
-										 break-words overflow-scroll sm:overflow-hidden"
-						style={{ userSelect: 'none' }}
-					>
-						{currency?.name || 'Loading'}
-					</a>
-					<p
-						className="text-[10px] text-center 
-					text-dark-purple-neko font-bold max-w-[auto] break-words"
-					>
-						{currency?.symbol.toUpperCase() || '...'}
-					</p>
-				</section>
-			</td>
-
-			<td className="table--body overflow-scroll sm:overflow-auto">
-				{formatterMoney(
-					'en-US',
-					{
-						style: 'currency',
-						currency: 'USD'
-					},
-					currency?.current_price || 0
-				)}
-			</td>
-			<td
-				className={`table--body ${
-					currency?.price_change_percentage_1h_in_currency || 0 > 0
-						? 'text-green-500'
-						: 'text-red-600'
-				}`}
-			>
-				{currency?.price_change_percentage_1h_in_currency &&
-					currency?.price_change_percentage_1h_in_currency.toFixed(2)}
-				%
-			</td>
-			<td
-				className={`table--body ${
-					currency?.market_cap_change_percentage_24h || 0 > 0
-						? 'text-green-500'
-						: 'text-red-600'
-				}`}
-			>
-				{(currency?.market_cap_change_percentage_24h &&
-					currency?.market_cap_change_percentage_24h.toFixed(2)) ||
-					'0.00'}
-				%
-			</td>
-			<td
-				className={`table--body  ${
-					currency?.price_change_percentage_7d_in_currency || 0 > 0
-						? 'text-green-500'
-						: 'text-red-600'
-				}`}
-			>
-				{(currency?.price_change_percentage_7d_in_currency &&
-					currency.price_change_percentage_7d_in_currency.toFixed(2)) ||
-					'0.00'}
-				%
-			</td>
-			<td className="table--body">
-				{formatterMoney(
-					'en-US',
-					{
-						style: 'currency',
-						currency: 'USD'
-					},
-					currency?.market_cap || 0
-				)}
-			</td>
-		</tr>
 	);
 };
